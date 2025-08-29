a64ae80ab859791e4bfaffdd36763e776a13805a
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-08-29 14:51:32 -0300

    Add necessary files to run configure

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -e
 
+OS=$(uname | tr '[:upper:]' '[:lower:]')
+
+# macOS uses glibtoolize
+if [ "$OS" = "darwin" ]; then
+    LIBTOOLIZE=glibtoolize
+else
+    LIBTOOLIZE=libtoolize
+fi
+
 clean() {
     echo -n "Cleaning created files... "
     rm -rf autom4te.cache aclocal.m4 Makefile.in src/Makefile.in m4/* \
@@ -13,10 +22,8 @@ clean() {
 
 dist() {
     echo -n "Cleaning temporary files... "
-    rm -rf autom4te.cache aclocal.m4 Makefile.in src/Makefile.in m4/* \
-        src/.deps src/.libs config.status compile config.guess \
-        config.sub depcomp install-sh ltmain.sh missing \
-        config.log libtool Makefile src/Makefile src/*.o src/.depend \
+    rm -rf autom4te.cache aclocal.m4 m4/* src/.deps src/.libs config.status \
+        depcomp config.log libtool Makefile src/Makefile src/*.o src/.depend \
         .depend config.h.in* config.h stamp-h1 src/hping3
     echo "done."
 }
@@ -26,13 +33,13 @@ build() {
     # Create config.h.in
     autoheader
     # Create libtool files
-    glibtoolize --force
+    $LIBTOOLIZE --force --copy
     # Create aclocal macros
     aclocal
     # Create configure
     autoconf
     # Create Makefile.in
-    automake --add-missing
+    automake --add-missing --copy
     echo "done."
 }
 
